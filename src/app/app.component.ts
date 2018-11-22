11e98561751e03909dbe4198fc3296eb8f8e5527
linfengisep <wang.linfeng0213@gmail.com>
2018-11-22 22:21:19 +0100

    angular test module,databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projetAngular';
+   isAuth = false;
+   lastUpdate = new Promise((resolve,reject)=>{
+   const date = new Date();
+   setTimeout(()=>{resolve(date);},2000);
+   });
+
+   appareilOne = 'Machine à laver';
+   appareilTwo = 'Frigo';
+   appareilThree = 'Ordinateur';
+
+   appareilStatusOn = "allumé"
+   appareilStatusOff = "éteint"
+//define an array manuelly
+   appareils = [
+      {
+         name:'machine à café',
+         status:'éteint'
+      },
+      {
+         name:'machine à laver',
+         status:'allumé'
+      },
+      {
+         name:'machine à verseille',
+         status:'éteint'
+      }
+   ];
+
+   constructor(){
+      setTimeout(
+         ()=>{
+               this.isAuth = true;
+            },4000
+      );
+   }
+   onLight(){
+      console.log("tout allumé.");
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomComponentComponent } from './custom-component/custom-component.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomComponentComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
